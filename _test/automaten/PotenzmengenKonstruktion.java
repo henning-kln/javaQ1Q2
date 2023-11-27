6bf6d9ea03842f715a1b8ac0a03569f06b618519
Henning Klein <info@henningklein.de>
2023-11-27 11:27:55 +0100

    PotenzmengenKonstruktion

---
+++ _test/automaten/PotenzmengenKonstruktion.java
@@ -0,0 +1,87 @@
+package _test.automaten;
+
+import java.util.HashMap;
+import java.util.TreeSet;
+
+import linear.List;
+import linear.ListWithViewer;
+
+public class PotenzmengenKonstruktion {
+	private Automat NEA;
+	private Automat ergebnisAutomat;
+	
+	public PotenzmengenKonstruktion() {
+		init();		
+		potenzmengenKonstruktion();
+		System.out.println();
+		
+		System.out.println("*** Deterministischer endlicher Automat (DEA) ***");
+		ergebnisAutomat.print();
+	}
+	
+	public void init() {
+		NEA = new Automat();
+		NEA.startzustandSetzen("q0");
+		
+		NEA = new Automat();
+		NEA.uebergangHinzufuegen("q0", "a", "q0,q1");
+		NEA.uebergangHinzufuegen("q0", "b", "q0");
+		NEA.uebergangHinzufuegen("q0", "x", "q0");
+		
+		NEA.uebergangHinzufuegen("q1", "a", "q2");
+		NEA.uebergangHinzufuegen("q1", "b", "q2");
+
+		NEA.uebergangHinzufuegen("q2", "a", "q3");
+		
+		NEA.uebergangHinzufuegen("q3", "a", "q3");
+		NEA.uebergangHinzufuegen("q3", "b", "q3");
+		NEA.uebergangHinzufuegen("q3", "x", "q3");
+		
+		NEA.startzustandSetzen("q0");
+		String[] endzustaende = {"q3"};
+		NEA.endzustaendeSetzen(endzustaende);		
+		System.out.println();
+		System.out.println("*** Nicht Deterministischer Endlicher Automat (NEA): ***");
+		System.out.println();
+		NEA.print();
+		
+	}
+	
+	public void potenzmengenKonstruktion() {
+		System.out.println("Potenzmengenkonstruktion wird durchgefuehrt");
+		ergebnisAutomat = new Automat();
+		Automat pruefAutomat = new Automat();
+		Automat fertigerAutomat = new Automat();
+		String startzustand = NEA.startzustand();
+		ergebnisAutomat.startzustandSetzen(startzustand);
+		List<String> zustandsListeErgebnis= new ListWithViewer<>();
+		String[] alphabet = NEA.alphabet();
+		zustandsListeErgebnis.append(NEA.startzustand());
+		pruefAutomat.zustandHinzufuegen(startzustand);
+		for(zustandsListeErgebnis.toFirst();zustandsListeErgebnis.hasAccess();zustandsListeErgebnis.next()){
+			String[] zustaendeAufgeiteilt = NEA.aufteilen(zustandsListeErgebnis.getContent());
+			for(int k=0; k<alphabet.length; k++){
+				String neuerLinkerEintrag = "";
+				for(int i=0;i<zustaendeAufgeiteilt.length;i++){
+					String folgeZustaendeVonAktuell = NEA.folgeZustaende(zustaendeAufgeiteilt[i], alphabet[k]);
+					neuerLinkerEintrag+=folgeZustaendeVonAktuell+",";
+				}
+				String gesauebert = NEA.alphabetischOrdnenUndTrennzeichenSaeubern(neuerLinkerEintrag);
+				ergebnisAutomat.uebergangHinzufuegen(zustandsListeErgebnis.getContent(), alphabet[k],gesauebert);
+				if(ergebnisAutomat.zustandVorhanden(gesauebert)){
+					continue;
+				}
+				zustandsListeErgebnis.append(gesauebert);
+				ergebnisAutomat.zustandHinzufuegen(gesauebert);
+			}
+			
+			
+		}
+	}
+	
+	public static void main(String[] args) {
+		new PotenzmengenKonstruktion();
+		
+	}
+	
+}
